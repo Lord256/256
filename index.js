dc00d84c9f172cbb4d39e9d85356a8dfe46057df
Lord256 <rafaelsousadasilva256@gmail.com>
2025-03-17 12:52:56 -0300

    Remover arquivos não necessários e atualizar .gitignore

---
+++ index.js
@@ -0,0 +1,56 @@
+const venom = require('venom-bot');
+const path = require('path');
+
+// Definindo o caminho da pasta de sessão
+const sessionFolder = path.join(__dirname, 'my-sessions');
+
+// Defina o caminho para o Chrome
+const chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"; // Caminho para o seu Chrome
+
+venom
+  .create('session-name', undefined, undefined, {
+    folderName: sessionFolder,
+    executablePath: chromePath,
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--headless',
+      '--disable-gpu',
+      '--window-size=1920x1080',
+    ],
+  })
+  .then((client) => start(client))
+  .catch((error) => console.log(error));
+
+function start(client) {
+  client.onMessage((message) => {
+    // Saudação do cliente
+    const saudacoes = ['oi', 'oi', 'Olá', 'Ola', 'ola', 'olá', 'Rafael', 'Rafa', 'Paulo', 'Bom dia', 'Boa tarde', 'Boa noite'];
+    if (saudacoes.includes(message.body.trim())) {
+      client.sendText(message.from,
+        "Seja bem-vindo! Sou o assistente virtual 🤖 do Rafael, consultor da Editora Lion. Escolha uma das opções abaixo para seguir o atendimento:\n\n" +
+        "1️⃣ Ver catálogo\n" +
+        "2️⃣ Falar com Rafael\n" +
+        "Digite o número da opção desejada."
+      );
+    } else if (message.body.toLowerCase() === 'menu') {
+      client.sendText(message.from,
+        "Seja bem-vindo! Sou o assistente virtual 🤖 do Rafael, consultor da Editora Lion. Escolha uma das opções abaixo para seguir o atendimento:\n\n" +
+        "1️⃣ Ver catálogo\n" +
+        "2️⃣ Falar com Rafael\n" +
+        "Digite o número da opção desejada."
+      );
+    } else if (message.body === '1') {
+      // Enviando o catálogo
+      client.sendText(message.from, "📚 Aqui está nosso catálogo: https://drive.google.com/drive/folders/1NYXeo1LEXhXY8WwCnacV99H73AIQhH_4")
+        .then(() => {
+          client.sendText(message.from, "Você pode fazer seu pedido diretamente através deste link: https://docs.google.com/spreadsheets/d/1HSyCu_NXYo6i6ssYVXu0IlKprF1fHPj1/edit?gid=787904471#gid=787904471");
+        });
+    } else if (message.body === '2') {
+      client.sendText(message.from, "👨‍💻 O Rafael entrará em contato em breve!");
+    } else {
+      client.sendText(message.from, "Não entendi. Digite 'menu' para ver as opções.");
+    }
+  });
+}
